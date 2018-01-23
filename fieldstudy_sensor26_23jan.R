6f337ba832358ce11aeb55d6004c355b37d9d70b
Jakob Flygare <jakob.flygare@ibm.com>
2018-01-23 15:32:50 +0100

    Sorterat i filen. La till library

--- fieldstudy_sensor26_23jan.R
+++ fieldstudy_sensor26_23jan.R
@@ -1,5 +1,5 @@
 #Fältstudie 2018-01-23 första sensor försöket nummer 26.
-
+library(plyr)
 
 #Läsa in filen
 sensor26jan23 <- read.csv("queryResults-23-jan.csv", header = FALSE) 
@@ -7,12 +7,21 @@ colnames(sensor26jan23) <- c("ID","Sensor Code","Time","Address","RSSI","OUI","T
 testdata <- subset(sensor26jan23,Time >=1516698600 & Time <=1516705200)
 idphase <- subset(sensor26jan23,Time >= 1516703500 & Time <=1516703680)
 uniqueOUI <- count(idphase,"OUI")
+uniqueAddress <- count(sensor26jan23,"Address")
+
+
 Simon <- "e581d79354d73e465709e1bd0282c702e7d45395"
 simondata <- subset(sensor26jan23,Address == Simon)
-jakobdata <- subset(sensor26jan23,Address == "5073d1ff497821b2469d8e7c5c48a4467b013f15")
-JakobOUI <- "3451C9"
+JakobMacBook <- "5073d1ff497821b2469d8e7c5c48a4467b013f15"
+jakobdata <- subset(sensor26jan23,Address == JakobMacBook)
 
-uniquemac <- count(idphase,"Address")
-uniquewhole <- count(sensor26jan23, "Address") 
-adresswholeset <- subset(sensor26jan23, Address == "5b38af7487d70082d994732d96922ecada233c60" )
-adress23jan <- subset(testdata, Address == "eb087a24b4d40bdf00c5f6d14924ac44adb369ad" )
+#Identify our devices and common devices in the data sets
+idphase_address <- count(idphase,"Address")
+whole_address <- count(sensor26jan23, "Address") 
+
+#Look up specific addresses
+addresswholeset <- subset(sensor26jan23, Address ==  address)
+address23jan <- subset(testdata, Address == "eb087a24b4d40bdf00c5f6d14924ac44adb369ad" )
+
+#Söka pingfrekvens för andra enheter
+address <- "c7824f65bad4a1d550df91951c1e27ec91a238e7"
