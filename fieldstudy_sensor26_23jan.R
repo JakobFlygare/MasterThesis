50998e7a046dad3a8eb9add54bc310cf1eb601c6
Jakob Flygare <jakob.flygare@ibm.com>
2018-01-23 15:08:49 +0100

    Identiferat simons mobil och min MacBook

--- fieldstudy_sensor26_23jan.R
+++ fieldstudy_sensor26_23jan.R
@@ -5,7 +5,14 @@
 sensor26jan23 <- read.csv("queryResults-23-jan.csv", header = FALSE) 
 colnames(sensor26jan23) <- c("ID","Sensor Code","Time","Address","RSSI","OUI","TS_PARSED")
 testdata <- subset(sensor26jan23,Time >=1516698600 & Time <=1516705200)
-uniquemac <- count(testdata,"Address")
+idphase <- subset(sensor26jan23,Time >= 1516703500 & Time <=1516703680)
+uniqueOUI <- count(idphase,"OUI")
+Simon <- "e581d79354d73e465709e1bd0282c702e7d45395"
+simondata <- subset(sensor26jan23,Address == Simon)
+jakobdata <- subset(sensor26jan23,Address == "5073d1ff497821b2469d8e7c5c48a4467b013f15")
+JakobOUI <- "3451C9"
+
+uniquemac <- count(idphase,"Address")
 uniquewhole <- count(sensor26jan23, "Address") 
-adresswholeset <- subset(sensor26jan23, Address == "e169e5fe25b6097c669ab186d30a4dd02d452ad4" )
-adress23jan <- subset(testdata, Address == "e169e5fe25b6097c669ab186d30a4dd02d452ad4" )
+adresswholeset <- subset(sensor26jan23, Address == "5b38af7487d70082d994732d96922ecada233c60" )
+adress23jan <- subset(testdata, Address == "eb087a24b4d40bdf00c5f6d14924ac44adb369ad" )
