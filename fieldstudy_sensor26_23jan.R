8426b687f42df8bac1ad14924f8944c89691c021
simondanielsson <simon.danielsson@ibm.com>
2018-01-24 11:03:05 +0100

    No change

--- fieldstudy_sensor26_23jan.R
+++ fieldstudy_sensor26_23jan.R
@@ -1,8 +1,10 @@
 #Fältstudie 2018-01-23 första sensor försöket nummer 26.
 install.packages("plyr")
 install.packages("ggplot2")
+install.packages("dplyr")
 library(plyr)
 library(ggplot2)
+library(dplyr)
 
 #Läsa in filen
 sensor26jan23 <- read.csv("queryResults-23-jan.csv", header = FALSE) 
@@ -12,9 +14,12 @@ idphase <- subset(sensor26jan23,Time >= 1516703500 & Time <=1516703680)
 uniqueOUI <- count(idphase,"OUI")
 uniqueAddress <- count(sensor26jan23,"Address")
 
-
+#Simon's data set and unique mac id 
 Simon <- "e581d79354d73e465709e1bd0282c702e7d45395"
 simondata <- subset(sensor26jan23,Address == Simon)
+#RSSI over time for Simon's data
+ggplot(simondata, aes(x=TS_PARSED, y=RSSI))+geom_point()
+
 JakobMacBook <- "5073d1ff497821b2469d8e7c5c48a4467b013f15"
 jakobdata <- subset(sensor26jan23,Address == JakobMacBook)
 
@@ -30,5 +35,5 @@ address23jan <- subset(testdata, Address == "eb087a24b4d40bdf00c5f6d14924ac44adb
 address <- "c7824f65bad4a1d550df91951c1e27ec91a238e7"
 mostCommonDevice <- subset(sensor26jan23, Address == "c7824f65bad4a1d550df91951c1e27ec91a238e7")
 
-ggplot(sensor26jan23,aes(Time))+geom_histogram(binwidth = 200)
+ggplot(sensor26jan23,aes(Time))+geom_bar()
 ggplot(sensor26jan23,aes(RSSI))+geom_histogram()
