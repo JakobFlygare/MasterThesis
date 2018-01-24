8c5bef9cab24f009b05403cb49b6baf1e1acf5b3
simondanielsson <simon.danielsson@ibm.com>
2018-01-24 16:18:36 +0100

    Added times a person pinged to the data

--- PlotPingFreq.R
+++ PlotPingFreq.R
@@ -14,3 +14,6 @@ ggplot(aes(timeDiff), data = onlyIdent) + geom_histogram()+
 #Create new column with day, hour and minute
 sensor26jan23$day_hr_min <- substr(sensor26jan23$TS_PARSED, 1, 15)
 
+#Create Number of times pinged for each individual during the time interval 0 to 300
+nr_pings = add_tally(onlyIdent)
+ggplot(nr_pings,aes(n))+geom_histogram()
