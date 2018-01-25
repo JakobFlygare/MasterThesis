1189133293796d4c1c04e55a2a25c086cf8f1855
Jakob Flygare <jakob.flygare@ibm.com>
2018-01-25 13:16:41 +0100

    tog bort test 4 och fasd kommentarerena

--- raspberry_analysis.R
+++ raspberry_analysis.R
@@ -11,11 +11,10 @@ pingrasp <- raspdata %>%
 
 ggplot(pingrasp,aes(TimeLag)) + geom_histogram(binwidth=10) +xlab("Time between pings(sec)") +
   ylab("Count") +ggtitle("Histogram over time between pings from same device")
-# fasd
+
 mean(pingrasp$TimeLag)
 median(pingrasp$TimeLag)
 summary(pingrasp$TimeLag)
-#test4
 
 #Calculating how often a device is pinged in a 5-minute window
 intrvl <- 300 #interval set to 5 minutes
