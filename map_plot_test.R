9bf81e16b68f768c636426866633179f9ad455b3
simondanielsson <simon.danielsson@ibm.com>
2018-01-30 13:44:47 +0100

    Test plot for map projection of data points

---
+++ map_plot_test.R
@@ -0,0 +1,19 @@
+# loading the required packages
+install.packages("ggmap")
+library(ggplot2)
+library(ggmap)
+
+# creating a sample data.frame with your lat/lon points
+lat <- c(59.288373,59.293882)
+lon <- c(18.084965,18.079787)
+df <- as.data.frame(cbind(lon,lat))
+
+# getting the map
+mapgilbert <- get_map(location = c(lon = mean(df$lon), lat = mean(df$lat)), zoom = 15,
+                      maptype = "satellite", scale = 2)
+
+# plotting the map with some points on it
+ggmap(mapgilbert) +
+  geom_point(data = df, aes(x = lon, y = lat, fill = "red", alpha = 0.8), size = 3, shape = 21) +
+  guides(fill=FALSE, alpha=FALSE, size=FALSE)
+
