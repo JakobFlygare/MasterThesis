ef61925f804fc3dad07705b3590deb6a5500ca2a
simondanielsson <simon.danielsson@ibm.com>
2018-02-08 10:53:19 +0100

    Gjort test för plot på map och partikel filter. Oklart partikel filter dock så kan nog radera den.

--- map_plot_test.R
+++ map_plot_test.R
@@ -9,7 +9,7 @@ lon <- c(18.084965,18.079787)
 df <- as.data.frame(cbind(lon,lat))
 
 # getting the map
-mapgilbert <- get_map(location = c(lon = mean(df$lon), lat = mean(df$lat)), zoom = 15,
+mapgilbert <- get_map(location = c(lon = mean(df$lon), lat = mean(df$lat)), zoom = 16,
                       maptype = "satellite", scale = 2)
 
 # plotting the map with some points on it
