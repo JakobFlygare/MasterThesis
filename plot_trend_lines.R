64b954a70928cedc1a58b3f5f29c7e4a865a1405
simondanielsson <simon.danielsson@ibm.com>
2018-02-10 16:45:00 +0100

    Added one plot but not done

--- plot_trend_lines.R
+++ plot_trend_lines.R
@@ -14,9 +14,8 @@ filter_data$day_hr <- substr(filter_data$TS_PARSED, 1, 13)
 #Count number of unique devices within each day and hour for the sensors
 uniq_per_day_hour <- filter_data %>% 
   select(day_hr,Address,Sensor_Code) %>% 
-  unique() %>% 
   group_by(day_hr,Sensor_Code) %>% 
-  summarise(num = n())
+  summarise(num = n(),u_num=n_distinct(Address))
 
 #Add a column displaying only the hour of the day
 uniq_per_day_hour$h <- substr(uniq_per_day_hour$day_hr,11,13)
@@ -24,7 +23,7 @@ uniq_per_day_hour$h <- substr(uniq_per_day_hour$day_hr,11,13)
 #Create standard deviation, mean and confidence 95% for each hour of the day
 mean_sd_num_hr<- uniq_per_day_hour%>%
   group_by(h,Sensor_Code)%>%
-  summarise(sd = sd(num),mean_num = mean(num),n=n())%>%
+  summarise(sd = sd(u_num),mean_num = mean(u_num),n=n())%>%
   mutate(ci = sd/sqrt(n)*qt(0.975,df=n-1))
 
 #Plot the mean and 95% confidence interval for average number of unique devices every hour of the day
@@ -43,4 +42,7 @@ ggplot(uniq_per_day_hour,aes(x=day_hr, y=num,colour = factor(Sensor_Code)))+
   xlab("Day and Hour")+
   ylab("Number of Unique Devices")
 
-
+#Not finished plot. Want to show line for all devices and unique devices in same plot
+ggplot(uniq_per_day_hour,aes(x=day_hr,group=Sensor_Code))+
+  geom_line(aes(y=num,colour = "All Devices"))+
+  geom_line(aes(y=u_num,colour = "Unique Devices"))
