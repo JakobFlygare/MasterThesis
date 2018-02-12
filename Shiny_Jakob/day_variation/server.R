2d6ea24b4bdad52bdb5974a719bcf6d2c65c60c0
Jakob Flygare <jakob.flygare@ibm.com>
2018-02-12 11:49:31 +0100

    lk

---
+++ Shiny_Jakob/day_variation/server.R
@@ -0,0 +1,28 @@
+#setwd("/Users/jakob/Documents/R/MasterThesis/Shiny_Jakob/day_variation")
+library(shiny)
+library(tidyverse)
+## RUN OTHER SCRIPT BEFORE TO GENERATE DATA ##
+
+shinyServer(
+  function(input, output){
+    
+    output$myplot <- renderPlot({
+      #var is connected to var from ui.R, and need numeric since it's a string
+      #bins connected to number of bins the user wants
+      sensorctrl <- input$sensor
+      confctrl <- input$confbars
+      stdctrl <- input$stdsmooth
+      
+      ggplot(subset(mean_sd_num_hr,Sensor_Code %in% c(sensorctrl)), aes(x=h, y=mean_num, colour=factor(Sensor_Code),group=Sensor_Code)) + 
+        geom_errorbar(aes(ymin=mean_num-ci, ymax=mean_num+ci), width=0.5,alpha=1*(confctrl)) +
+        geom_smooth(alpha=0.5*(stdctrl)) +
+        #geom_line() +
+        geom_point() +
+        xlab("Hour") +
+        ylab("Average Number of Unique Devices")+
+        scale_colour_discrete(name  ="Sensor Code")
+    })    
+    
+  }
+  
+)
